931a168ea3588b21a73dec4e9bc353a0757ccd50
leonardosibela <leonardo_sibela@hotmail.com>
2017-09-20 18:19:35 +0000

    Adicionando calculos de valores

--- Hotel/MainWindow.xaml.cs
+++ Hotel/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,15 +16,86 @@ using System.Windows.Shapes;
 
 namespace Hotel
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
-
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void txtQtdDias_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            atualizarValor();
+        }
+
+
+
+        private void atualizarValor() {
+            if (txtQtdDias.Text.Length == 0)
+            {
+                lblTotal.Content = "R$0,00";
+                return;
+            }
+
+            double totalDiario = 0;
+            int qtdDias = Convert.ToInt32(txtQtdDias.Text);
+
+            totalDiario += radNormal.IsChecked == true ? 100 : 0;
+            totalDiario += radLuxo.IsChecked == true ? 150 : 0;
+            totalDiario += radSuperLuxo.IsChecked == true ? 200 : 0;
+
+            totalDiario += chkArCondicionado.IsChecked == true ? 50 : 0;
+            totalDiario += chkHidromassagem.IsChecked == true ? 40 : 0;
+            totalDiario += chkWifi.IsChecked == true ? 30 : 0;
+
+            double total = totalDiario * qtdDias;
+            lblTotal.Content = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", total);
+        }
+
+        private void radNormal_Checked(object sender, RoutedEventArgs e)
+        {
+            atualizarValor();
+        }
+
+        private void radLuxo_Checked(object sender, RoutedEventArgs e)
+        {
+            atualizarValor();
+        }
+
+        private void radSuperLuxo_Checked(object sender, RoutedEventArgs e)
+        {
+            atualizarValor();
+        }
+
+        private void chkArCondicionado_Checked(object sender, RoutedEventArgs e)
+        {
+            atualizarValor();
+        }
+
+        private void chkArCondicionado_Unchecked(object sender, RoutedEventArgs e)
+        {
+            atualizarValor();
+        }
+
+        private void chkHidromassagem_Checked(object sender, RoutedEventArgs e)
+        {
+            atualizarValor();
+        }
+
+        private void chkHidromassagem_Unchecked(object sender, RoutedEventArgs e)
+        {
+            atualizarValor();
+        }
+
+        private void chkWifi_Checked(object sender, RoutedEventArgs e)
+        {
+            atualizarValor();
+        }
+
+        private void chkWifi_Unchecked(object sender, RoutedEventArgs e)
+        {
+            atualizarValor();
         }
     }
 }
